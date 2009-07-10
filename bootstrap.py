7a58a9a7920c54dc8ae152588b496ba085a74eba
Anders Pearson <anders@columbia.edu>
2009-07-10 14:16:36 -0400

    making it work on jaunty

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.5
 import os
 import sys
 import subprocess
@@ -10,5 +10,5 @@ vedir = os.path.join(pwd,"ve")
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"create-ve.py")])
-subprocess.call(["python",os.path.join(pwd,"ve-bootstrap.py"),vedir])
+subprocess.call(["python2.5",os.path.join(pwd,"create-ve.py")])
+subprocess.call(["python2.5",os.path.join(pwd,"ve-bootstrap.py"),vedir])
