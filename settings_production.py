c461de8627002c08d01013ee7bdebbe29f09672a
Anders Pearson <anders@columbia.edu>
2010-07-19 14:26:47 -0400

    support local_settings overrides

--- settings_production.py
+++ settings_production.py
@@ -8,3 +8,10 @@ MEDIA_ROOT = '/var/www/skinflint/uploads/'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
