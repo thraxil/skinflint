a1cd686c341bb7d618b1b7e9406df3e2b4c5764f
Anders Pearson <anders@columbia.edu>
2013-10-08 21:45:29 -0400

    template paths on prod

--- skinflint/settings_production.py
+++ skinflint/settings_production.py
@@ -2,7 +2,7 @@
 from settings_shared import *
 
 TEMPLATE_DIRS = (
-    "/var/www/skinflint/skinflint/templates",
+    "/var/www/skinflint/skinflint/skinflint/templates",
 )
 
 MEDIA_ROOT = '/var/www/skinflint/uploads/'
