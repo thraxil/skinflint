c1ac315eb8275c23ab393f09cd5de19fde321e97
Anders Pearson <anders@columbia.edu>
2014-05-10 16:17:05 -0400

    use makefile for deployment

--- fabfile.py
+++ fabfile.py
@@ -9,15 +9,14 @@ def restart_gunicorn():
     sudo("restart skinflint")
 
 def prepare_deploy():
-    local("./manage.py test")
+    local("make test")
 
 def deploy():
     code_dir = "/var/www/skinflint/skinflint"
     with cd(code_dir):
         run("git pull origin master")
-        run("./bootstrap.py")
-        run("./manage.py migrate")
-        run("./manage.py collectstatic --noinput --settings=skinflint.settings_production")
+        run("make migrate")
+        run("make collectstatic")
         for n in nginx_hosts:
             run(("rsync -avp --delete media/ "
                  "%s:/var/www/skinflint/skinflint/media/") % n)
